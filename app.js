3ebb99bb9c0a5ad590aac5f7bafaa54ceb7e871f
Ayush Singh <ayushsingh723585@gmail.com>
2024-12-20 20:30:40 +0530

    schema validations

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const methodOverride = require('method-override');
 const ejsMate = require('ejs-mate');
 const wrapAsync = require('./utils/wrapAsync.js');
 const ExpressError = require('./utils/ExpressError.js');
-
+const { listingSchema } = require('./schema.js');
 
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
@@ -32,8 +32,23 @@ app.get("/listings/new", (req, res) => {
     res.render("listings/new.ejs");
 });
 
+const validateListing = (req, res, next) => {
+    let{error} = listingSchema.validate(req.body);
+    if(error){
+        throw new ExpressError(result.error, 400);
+    } else {
+        next();
+    }
+}
+
 //create route
-app.post("/listings", wrapAsync(async (req, res,next) => {
+app.post("/listings",validateListing,
+    wrapAsync(async (req, res,next) => {
+    let result = listingSchema.validate(req.body);
+        console.log(result);
+        if(result.error){
+            throw new ExpressError(result.error.details.map(el => el.message).join(','), 400);
+        }
         const newListing = new Listing(req.body.listing);
         await newListing.save();
         res.redirect('/listings');
@@ -47,7 +62,8 @@ app.get("/listings/:id/edit", wrapAsync(async (req, res) => {
 }));
 
 //update route
-app.put("/listings/:id", wrapAsync( async (req, res) => {
+app.put("/listings/:id",validateListing,
+    wrapAsync( async (req, res) => {
     let {id} = req.params;
      await Listing.findByIdAndUpdate(id, req.body.listing);
     res.redirect(`/listings/${id}`);
@@ -65,7 +81,7 @@ app.delete("/listings/:id",  wrapAsync(async (req, res) => {
 app.get('/', (req, res) => {
     res.redirect('/listings'); // Redirect to listings route
 });
-
+//index route 
 app.get('/listings', wrapAsync(async (req, res) => {
     const allListings = await Listing.find({});
     res.render("listings/index.ejs", { allListings });
