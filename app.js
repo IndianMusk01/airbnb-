a03d28ee2ee6ae0aa70d1364b632e709ab922917
Ayush Singh <ayushsingh723585@gmail.com>
2025-01-07 18:07:59 +0530

    configuring passport authentication

--- app.js
+++ app.js
@@ -10,6 +10,9 @@ const ExpressError = require('./utils/ExpressError.js');
 const { listingSchema, reviewSchema } = require('./schema.js');
 const session = require('express-session');
 const flash = require('connect-flash');
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
+const User = require('./models/user.js');
 
 
 const app = express();
@@ -29,6 +32,13 @@ const sessionOptions = {
 
 app.use(session(sessionOptions));
 app.use(flash());
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.use((req,res,next)=>{
     res.locals.success = req.flash('success');
