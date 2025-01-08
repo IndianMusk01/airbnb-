82120906345fd10711c34a14f358f72abbe03ba4
Ayush Singh <ayushsingh723585@gmail.com>
2025-01-08 12:41:42 +0530

    LogIn user

--- app.js
+++ app.js
@@ -180,6 +180,14 @@ app.post("/signup",wrapAsync(async(req,res)=>{
         res.redirect('/listings');
     });
 }));
+//login route
+app.get("/login",(req,res)=>{
+    res.render("users/login.ejs");
+});
+app.post("/login",passport.authenticate('local',{failureFlash:true,failureRedirect:'/login'}),(req,res)=>{
+    req.flash('success', 'Welcome back!');
+    res.redirect('/listings');
+});
 
 // Root Route
 app.get('/', (req, res) => {
